5f6a37c039343f4d9487a5de8b2c417cf014b9bf
shihao <2544528304@qq.com>
2025-05-11 09:32:39 +0800

    .

--- ci/compose/mysql/init.sql
+++ ci/compose/mysql/init.sql
@@ -1,5 +1,5 @@
 -- 事务隔离级别
 set session transaction isolation level read committed;
 SET GLOBAL tx_isolation='READ-COMMITTED';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'Eisoo.com123' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
